3b08dc757b235afa985f9b9ffd94375f55f37f9e
karthikeyini-kalaimurugan <karthikeyini.kalaimurugan@freshworks.com>
2020-01-10 00:09:53 +0530

    added cards

--- index.js
+++ index.js
@@ -6,18 +6,87 @@ const crypto = require('crypto');
 const sharedSecret = "CrDKeWlKabngHJbqbSMFu/AIniR61xb83wUn+LrqHO8="; // e.g. "+ZaRRMC8+mpnfGaGsBOmkIFt98bttL5YQRq3p2tXgcE="
 const bufSecret = Buffer(sharedSecret, "base64");
 
+var request = require("request");
 var http = require('http');
 var PORT = process.env.port || process.env.PORT || 8080;
 
-http.createServer(function(request, response) { 
+const sendPayload = function(){
+	var options = { method: 'POST',
+	  url: 'https://outlook.office.com/webhook/1d8f5e75-6e0e-4e8e-9432-eb48d2550a02@221ba5e5-7011-40b1-bf29-b717749b7124/IncomingWebhook/19dcc0af4fd141fbb3e2d202af1e3cee/bedd77d6-353d-4858-8ffc-458d8733d523',
+	  headers: 
+	   { 'Content-Type': 'application/json' },
+	  body: 
+	   { title: 'Fill Details to create a Ticket',
+		 text: '* fields are mandatory.',
+		 '@type': 'MessageCard',
+		 '@context': 'http://schema.org/extensions',
+		 themeColor: '0076D7',
+		 summary: 'Larry Bryant created a new task',
+		 sections: 
+		  [ { activityTitle: '![TestImage](https://47a92947.ngrok.io/Content/Images/default.png)Larry Bryant created a new task',
+			  activitySubtitle: 'On Project Tango',
+			  activityImage: 'https://teamsnodesample.azurewebsites.net/static/img/image5.png',
+			  facts: 
+			   [ { name: 'Assigned to', value: 'Unassigned' },
+				 { name: 'Due date',
+				   value: 'Mon May 01 2017 17:07:18 GMT-0700 (Pacific Daylight Time)' },
+				 { name: 'Status', value: 'Not started' } ],
+			  markdown: true } ],
+		 potentialAction: 
+		  [ { '@type': 'ActionCard',
+			  name: 'Add a comment',
+			  inputs: 
+			   [ { '@type': 'TextInput',
+				   id: 'comment',
+				   isMultiline: false,
+				   title: 'Add a comment here for this task' } ],
+			  actions: 
+			   [ { '@type': 'HttpPOST',
+				   name: 'Add comment',
+				   target: 'http://...' } ] },
+			{ '@type': 'ActionCard',
+			  name: 'Set due date',
+			  inputs: 
+			   [ { '@type': 'DateInput',
+				   id: 'dueDate',
+				   title: 'Enter a due date for this task' } ],
+			  actions: 
+			   [ { '@type': 'HttpPOST',
+				   name: 'Save',
+				   target: 'https://www.google.com' } ] },
+			{ '@type': 'ActionCard',
+			  name: 'Change status',
+			  inputs: 
+			   [ { '@type': 'MultichoiceInput',
+				   id: 'list',
+				   title: 'Select a status',
+				   isMultiSelect: 'false',
+				   choices: 
+					[ { display: 'In Progress', value: '1' },
+					  { display: 'Active', value: '2' },
+					  { display: 'Closed', value: '3' } ] } ],
+			  actions: [ { '@type': 'HttpPOST', name: 'Save', target: 'http://...' } ] } ] },
+	  json: true };
+	
+	request(options, function (error, response, body) {
+	  if (error) throw new Error(error);
+	
+	  console.log(body);
+	});
+	
+}
+
+
+http.createServer(function(req, response) { 
 	var payload = '';
-	// Process the request
-	request.on('data', function (data) {
+	// Process the req
+	req.on('data', function (data) {
 		payload += data;
 	});
 	
-	// Respond to the request
-	request.on('end', function() {
+	// Respond to the req
+	req.on('end', function() {
+		sendPayload();
 		try {
 			// Retrieve authorization HMAC information
 			var auth = this.headers['authorization'];
